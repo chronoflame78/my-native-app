af86c88672635d6e5efd3429ea9c8719fcdba60a
NhatNguyen <nhatnguyen@lgcns.com>
2025-01-20 13:56:56 +0700

    update

--- GameComponent.js
+++ GameComponent.js
@@ -1,34 +1,77 @@
 import React, { useEffect, useRef } from "react";
 import { StyleSheet, View, Image } from "react-native";
 import Canvas, { Image as CanvasImage } from "react-native-canvas";
-import imagePng from './img/PelletTown.png'
+import pelletTown from "./img/PelletTown.png";
+import playerDown from "./img/playerDown.png";
+
+class Sprite {
+  constructor({ position, velocity, image, context }) {
+    this.position = position;
+    this.image = image;
+    this.context = context;
+  }
+
+  draw() {
+    this.context?.drawImage(this.image, this.position.x, this.position.y);
+  }
+}
 
 const CanvasComponent = () => {
-  const handleCanvas = async (canvas) => {
-    if (canvas) {
-      const context = canvas.getContext("2d");
+  const canvasRef = useRef(null);
+  const mapImageRef = useRef();
+  const playerImageRef = useRef();
+  const backgroundRef = useRef(new Sprite({ position: { x: -785, y: -650 } }));
 
-      // Set canvas dimensions
-      canvas.width = 1024;
-      canvas.height = 576;
+  const animate = () => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    const mapImage = mapImageRef.current;
+    const playerImage = playerImageRef.current;
+    let background = backgroundRef.current;
 
-      // Fill the canvas with a white background
-      context.fillStyle = "white";
-      context.fillRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(animate);
+    background = new Sprite({
+      position: { x: -785, y: -650 },
+      image: mapImage,
+      context: context,
+    });
+    background.draw();
 
-      const image = new CanvasImage(canvas);
-      const imageUri = Image.resolveAssetSource(imagePng).uri;
-      image.src = imageUri;
-
-      image.addEventListener("load", () => {
-        context.drawImage(image, -750, -750);
-      });
-    }
+    context?.drawImage(
+      playerImage,
+      0,
+      0,
+      playerImage.width / 4,
+      playerImage.height,
+      canvas.width / 2 - playerImage.width / 4,
+      canvas.height / 2 - playerImage.height / 2,
+      playerImage.width / 4,
+      playerImage.height
+    );
   };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    canvas.width = 1024;
+    canvas.height = 576;
+
+    context.fillStyle = "white";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    mapImageRef.current = new CanvasImage(canvas);
+    mapImageRef.current.src = Image.resolveAssetSource(pelletTown).uri;
+
+    playerImageRef.current = new CanvasImage(canvas);
+    playerImageRef.current.src = Image.resolveAssetSource(playerDown).uri;
+
+    animate();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Canvas ref={handleCanvas} />
+      <Canvas ref={canvasRef} />
     </View>
   );
 };
